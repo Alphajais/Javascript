bacd8605964c6dd5dbcdcac6ce73fcd3ff3f6670
Alphajais <142743971+Alphajais@users.noreply.github.com>
2024-05-29 19:57:35 +0000

    container config done

---
+++ 01_basics/string.js
@@ -0,0 +1,14 @@
+const name = new String('Atul-jaiswal-is-goodboy')
+console.log(name);
+console.log(name.length);
+console.log(name.indexOf('u'));
+const newName = "   Atul"
+console.log(newName);
+console.log(newName.trim());// it used to delete the extra space in word
+const url = "atul://jaiswal@.com://%20"
+console.log(url.replace('%20','-'));
+console.log(name.split('-'));
+
+
+
+
